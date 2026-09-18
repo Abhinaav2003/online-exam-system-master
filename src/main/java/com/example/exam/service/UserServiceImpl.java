d8c31c90274ed931be7b58a46265361181b57ab4
Abhinaav2003 <144618349+Abhinaav2003@users.noreply.github.com>
2026-09-18 18:23:03 +0530

    A

--- src/main/java/com/example/exam/service/UserServiceImpl.java
+++ src/main/java/com/example/exam/service/UserServiceImpl.java
@@ -7,7 +7,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService 
+{
 
     @Autowired
     private UserRepository userRepository;
@@ -16,14 +17,16 @@ public class UserServiceImpl implements UserService {
     private PasswordEncoder passwordEncoder;
 
     @Override
-    public void saveStudent(User user) {
+    public void saveStudent(User user) 
+    {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setRole("ROLE_STUDENT");
         userRepository.save(user);
     }
 
     @Override
-    public User findByUsername(String username) {
+    public User findByUsername(String username) 
+    {
         return userRepository.findByUsername(username).orElse(null);
     }
 }
